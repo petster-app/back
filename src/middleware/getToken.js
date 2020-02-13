55d8b60c3267415d174507121c3641ef503c0469
Lillian Gales <lilliangales@ymail.com>
2020-02-13 14:40:51 -0800

    modified the routes on search and temp router

--- src/middleware/getToken.js
+++ src/middleware/getToken.js
@@ -6,7 +6,6 @@ module.exports = (request, response, next) => {
     .send({'grant_type': 'client_credentials', 'client_id' : `${process.env.PET_FINDER_API_KEY}`, 'client_secret' : `${process.env.PET_FINDER_SECRET}`})
     .set('Content-Type', 'application/x-www-form-urlencoded')
     .then(data => {
-      // console.log(data)
       request.token = data.body.access_token
       next();
       return data
