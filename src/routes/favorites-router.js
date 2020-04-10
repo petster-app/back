07fecd7c644b48947abcb9e4afbb0ec7169a860e
Lillian Gales <lilliangales@ymail.com>
2020-04-10 00:06:48 -0700

    testing

--- src/routes/favorites-router.js
+++ src/routes/favorites-router.js
@@ -1,49 +1,62 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const pg = require('pg');
+const express = require("express");
+const pg = require("pg");
 const router = express.Router();
-const handleError = require('../middleware/error');
+const handleError = require("../middleware/error");
 
-const client = new pg.Client(process.env.DATABASE_URL)
+const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 
-router.post('/favorites', postFavorite);
-router.get('/favorites', getAllFavoritePets);
-router.get('/favorites/:userName', getFavoritePets);
-router.delete('/favorites', deleteFavorite);
+router.post("/favorites", postFavorite);
+router.get("/favorites", getAllFavoritePets);
+router.get("/favorites/:userName", getFavoritePets);
+router.delete("/favorites", deleteFavorite);
 
-function postFavorite(request, response){
-  let { petfinderid, userName, type, name, age, gender, size, city, state, description, photo, url } = request.body;
+function postFavorite(request, response) {
+  let {
+    petfinderid,
+    userName,
+    type,
+    name,
+    age,
+    gender,
+    size,
+    city,
+    state,
+    description,
+    photo,
+    url,
+  } = request.body;
   const SQL = `
   INSERT INTO favorited_pets (petfinderid, type, name, age, gender, size, city, state, description, photo, url) SELECT '${petfinderid}','${type}','${name}', '${age}', '${gender}', '${size}','${city}', '${state}', '${description}', '${photo}', '${url}' 
   WHERE NOT EXISTS (SELECT * FROM favorited_pets WHERE petfinderid = '${petfinderid}');
   INSERT INTO user_pets (pet_id, username_id) SELECT '${petfinderid}',(SELECT id FROM users WHERE username='${userName}')
   WHERE NOT EXISTS (SELECT * FROM user_pets WHERE pet_id = '${petfinderid}' AND username_id=(SELECT id FROM users WHERE username='${userName}'));
   `;
-  return client.query(SQL)
+  return client
+    .query(SQL)
     .then(response.send(request.body))
-    .catch(error => handleError(error, response));
+    .catch((error) => handleError(error, response));
 }
 
-
 function getAllFavoritePets(request, response) {
-
   let userName = request.params.userName;
   let SQL = `
     SELECT * FROM favorited_pets;
   `;
 
-  return client.query(SQL)
-    .then(sqlResults => {
+  return client
+    .query(SQL)
+    .then((sqlResults) => {
       response.send(sqlResults.rows);
     })
-    .catch(error => handleError(error, response));
+    .catch((error) => handleError(error, response));
 }
 
 function getFavoritePets(request, response) {
-
   let userName = request.params.userName;
+  console.log(userName, "username");
 
   let SQL = `
     SELECT * FROM favorited_pets
@@ -54,25 +67,25 @@ function getFavoritePets(request, response) {
     WHERE username='${userName}';
   `;
 
-  return client.query(SQL)
-    .then(sqlResults => {
+  return client
+    .query(SQL)
+    .then((sqlResults) => {
       response.send(sqlResults.rows);
     })
-    .catch(error => handleError(error, response));
+    .catch((error) => handleError(error, response));
 }
 
-
 function deleteFavorite(request, response) {
-
   const userName = request.body.username;
   const petfinderid = request.body.petfinderid;
- 
-  const SQL = `DELETE FROM user_pets WHERE pet_id='${petfinderid}' AND username_id=(SELECT id FROM users WHERE username='${userName}');`
-  return client.query(SQL)
-  .then(sqlResults => {
-    response.send(sqlResults.rows);
-  })
-.catch(err => handleError(err, response));
+
+  const SQL = `DELETE FROM user_pets WHERE pet_id='${petfinderid}' AND username_id=(SELECT id FROM users WHERE username='${userName}');`;
+  return client
+    .query(SQL)
+    .then((sqlResults) => {
+      response.send(sqlResults.rows);
+    })
+    .catch((err) => handleError(err, response));
 }
 
 module.exports = router;
